e12d6fa3fb176eed1498faff7d5363719802d80e
João Dyonisio Dias Cruz <dyonisiojoao0@gmail.com>
2026-02-02 19:08:01 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Se você está vendo isso, parabéns você é curioso!</h1>
+    return <h1>Se você está vendo isso, parabéns você é curioso! 😎</h1>
 }
 
 export default Home;
