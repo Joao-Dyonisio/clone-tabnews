0529ce25c24628e11f079144b88fb1d4c10e2058
João Dyonisio Dias Cruz <dyonisiojoao0@gmail.com>
2026-02-05 19:33:15 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
 function Home() {
-    return <h1>Clara,eu amo você. Se você me ama, dá uma risadinha!</h1>
+  return <h1>Clara,eu amo você. Se você me ama, dá uma risadinha!</h1>;
+}
+
+function teste() {
+  console.log("teste");
+}
+
+function teste2() {
+  console.log("teste2");
 }
 
 export default Home;
